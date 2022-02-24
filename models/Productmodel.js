3c9ce01d395a5ce54a718843f85496b77131edd3
Tuva-Li Wass Vallin <tuvali.wahlin@icloud.com>
2022-02-24 14:27:11 +0100

    .

--- models/Productmodel.js
+++ models/Productmodel.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema({
   name: { type: String, required: true },
   imgUrl: { type: String, required: true },
-  price: { type: Number, required: true },
   description: { type: String, required: true },
 });
 
