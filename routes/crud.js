1fffb246d483ff08ac406db8de7f2dd4a5f836d5
Holmkvists <adamholmkvists@gmail.com>
2022-02-27 21:58:17 +0100

    simple update added

--- routes/crud.js
+++ routes/crud.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const ArtworkModel = require("../models/Artworkmodel");
 const router = express.Router();
 
 // CREATE - POST ARTWORK
 router.get("/post", (req, res) => {
-  res.render("post-edit");
+  res.render("crud/create");
 });
 
 router.post("/post", async (req, res) => {
@@ -19,6 +20,26 @@ router.post("/post", async (req, res) => {
 
   await newArtwork.save();
   res.redirect("/post");
+});
+
+// UPDATE - EDIT ARTWORK
+
+router.get("/artworks/:id/edit", async (req, res) => {
+  const artwork = await ArtworkModel.findById(req.params.id);
+
+  res.render("crud/update", artwork);
+});
+
+router.post("/artworks/:id/edit", async (req, res) => {
+  const { name, imgUrl, description } = req.body;
+
+  await ArtworkModel.findByIdAndUpdate(req.params.id, {
+    name,
+    imgUrl,
+    description,
+  });
+
+  res.redirect("/");
 });
 
 module.exports = router;
