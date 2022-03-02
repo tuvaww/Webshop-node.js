f3960013adcadfbb9f1ce389b8485db1a63c0855
Holmkvists <adamholmkvists@gmail.com>
2022-03-02 12:49:19 +0100

    removed unneccesary files

---
+++ models/Productmodel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  imgUrl: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Product", productSchema);
