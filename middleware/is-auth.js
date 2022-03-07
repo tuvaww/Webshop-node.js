ddf6324c9a6943091516c62a228804950f0e64c1
Tuva-Li Wass Vallin <tuvali.wahlin@icloud.com>
2022-03-07 16:39:13 +0100

    small edit in middleware

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,6 @@
 exports.authUserPages = (req, res, next) => {
   if (!req.session.isLoggedIn) {
-    return res.render("login", {
+    return res.render("auth/login", {
       accessUserPages: "You need to login for access",
     });
   }
--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const ArtModel = require("../models/Artworkmodel");
 const Usermodel = require("../models/Usermodel");
 
-/* const isAuth = require("../middleware/is-auth");
- */
+const isAuth = require("../middleware/is-auth");
 
-router.get("/saved", async (req, res) => {
+router.get("/saved", isAuth.authUserPages, async (req, res) => {
   req.user
     .populate("savedFavorite.items.artId")
     .then((user) => {
