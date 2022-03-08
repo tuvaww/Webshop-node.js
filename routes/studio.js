99f06d50d5605ca7060c845b843247c9c07a68af
Holmkvists <adamholmkvists@gmail.com>
2022-03-08 17:31:33 +0100

    cleaned code

--- routes/studio.js
+++ routes/studio.js
@@ -6,18 +6,13 @@ const ArtworkModel = require("../models/Artworkmodel");
 router.get("/", async (req, res) => {
   const artworks = await ArtworkModel.find().limit(3).lean();
 
-  res.render("home", {
-    art: artworks,
-    // loggedInUser: req.session.isLoggedIn,
-  });
+  res.render("home", { artworks });
 });
 
 router.get("/artworks", async (req, res) => {
   const artworks = await ArtworkModel.find().lean();
-  res.render("artworks/artworks", {
-    //  isLoggedIn: req.session.isLoggedIn,
-    artworks: artworks,
-  });
+
+  res.render("artworks/artworks", { artworks });
 });
 
 module.exports = router;
