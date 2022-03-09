2b58c7760c797628e12216c2f92bed4f3de2b53a
Tuva-Li Wass Vallin <tuvali.wahlin@icloud.com>
2022-03-09 11:50:37 +0100

    small fix

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,7 +1,7 @@
 exports.authUserPages = (req, res, next) => {
   if (!req.session.isLoggedIn) {
     return res.render("auth/login", {
-      accessUserPages: "You need to login for access",
+      accessUserPages: "401.You need to login for access",
     });
   }
 
