411b9ba0c2d62c5c1551313049f97f4dff8d17b2
Emma <91128612+EmmaNGagen@users.noreply.github.com>
2022-03-09 11:16:10 +0100

    profile page

--- routes/profile.js
+++ routes/profile.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const ArtworkModel = require("../models/Artworkmodel.js");
 const UserModel = require("../models/Usermodel.js");
-const utils = require("../utils");
-const isAuth = require("../middleware/is-auth");
+const middlewares = require("../middleware/is-auth");
 
-router.get("/profile", isAuth.authUserPages, async (req, res) => {
-  const user = await UserModel.findById(req.params.id).lean();
-  const artwork = await ArtworkModel.find({ user: req.params.id }).lean();
+router.get("/profile", middlewares.authUserPages, async (req, res) => {
+  const user = req.user.username;
+  const id = req.user._id;
+  const art = ArtworkModel.find({ id });
 
-  res.render("auth/profile", { user, artwork });
+  res.render("auth/profile", { artworks: art, user, id });
 });
 
 module.exports = router;
