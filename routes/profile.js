73fbe29af775049f2d047a276fa2c894bf88a44b
Emma <91128612+EmmaNGagen@users.noreply.github.com>
2022-03-09 11:49:16 +0100

    await på artworkModel

--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const ArtworkModel = require("../models/Artworkmodel.js");
-const UserModel = require("../models/Usermodel.js");
 const middlewares = require("../middleware/is-auth");
 
 router.get("/profile", middlewares.authUserPages, async (req, res) => {
   const user = req.user.username;
   const id = req.user._id;
-  const art = ArtworkModel.find({ id });
+  const art = await ArtworkModel.find({ id });
 
   res.render("auth/profile", { artworks: art, user, id });
 });
