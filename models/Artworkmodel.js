afbf8368a69927503044513a5c6f80198cf83d51
Tuva-Li Wass Vallin <tuvali.wahlin@icloud.com>
2022-03-13 17:18:59 +0100

    wishlist fix

--- models/Artworkmodel.js
+++ models/Artworkmodel.js
@@ -5,7 +5,6 @@ const artworkSchema = new Schema({
   name: { type: String, required: true },
   imgUrl: { type: String, required: true },
   description: { type: String, required: true },
-  //produkt som tillhör user med id.., ref är från vilken colection
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
